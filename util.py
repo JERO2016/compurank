f0be3dd9993daab1571373ea29c85a4fd6236f54
Jesus <j.ramirezdibstoys@gmail.com>
2026-01-17 02:07:59 -0600

    Refactor palindrome logic to update flag variable

--- util.py
+++ util.py
@@ -1,8 +1,13 @@
-def is_palindrome(str_to_evaluate):
-    cleaned = ""    #cadena vacía para guardar lo que tengamos de resultado
+def is_palindrome(str_to_evaluate):         # We define the function is_palindrome
+    is_palindrome = False                   # Initially, we assume that is_palindrome is false and we will try to prove otherwise.
     
-    for char in str_to_evaluate.lower():
-        if char.isalnum():      #es letra o número, se queda. cualquier otra cosa se va
-            cleaned += char     #ya que se quedó, lo guardamos en cleaned
-            
-    return cleaned == cleaned[::-1]     #compara lo que quedó al final en cleaned y lo lee en reversa para decir si es T o F
+    cleaned = ""                            # We create a variable with an empty string where we will store the later result.
+                                            # WE START CLEANING
+    for char in str_to_evaluate.lower():    # Convert each caracter of the string to evaluate to lowercase.
+        if char.isalnum():                  # Determine whether it is a letter or a number, or if it is a special character. If so, remove it.   
+            cleaned += char                 # The result will be sent to the empty variable created previously.
+                                            # WE COMPARE
+    if cleaned == cleaned[::-1]:            # If the text of the empty string reads the same from left to right or viceversa.
+            is_palindrome = True            # If so, then it states that it is a palindrome
+    
+    return is_palindrome                    # Returns the response to the program to indicate whether it is a palindrome

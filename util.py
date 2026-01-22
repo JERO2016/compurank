07b4f9d124c7b6f86d2d875411aa4d88476e3815
Jesus <j.ramirezdibstoys@gmail.com>
2026-01-22 00:18:01 -0600

    Add palindrome validation function

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
-def is_palindrome(str_to_evaluate):         # We define the function is_palindrome
-    is_palindrome = False                   # Initially, we assume that is_palindrome is false and we will try to prove otherwise.
-    
+def is_palindrome(str_to_evaluate):         # Palindrome is a word that can be read from left to right and viceversa and retains its meaning.
+    is_palindrome = False                   
+   
     cleaned = ""                            # We create a variable with an empty string where we will store the later result.
                                             # WE START CLEANING
     for char in str_to_evaluate.lower():    # Convert each caracter of the string to evaluate to lowercase.
@@ -11,3 +11,39 @@ def is_palindrome(str_to_evaluate):         # We define the function is_palindro
             is_palindrome = True            # If so, then it states that it is a palindrome
     
     return is_palindrome                    # Returns the response to the program to indicate whether it is a palindrome
+
+
+def is_fizzbuzz(number):
+    pass
+
+
+def is_prime(number):
+    pass
+
+
+def is_leap_year(year):
+    pass
+
+
+def do_calculator(expression):
+    pass
+
+
+def generate_difference_between_dates(date1, date2, unit):
+    pass
+
+
+def vector_addition(vec1, vec2):
+    pass
+
+
+def vector_multiplication(vec, scalar):
+    pass
+
+
+def matrix_addition(mat1, mat2):
+    pass
+
+
+def matrix_multiplication(mat1, mat2):
+    pass
